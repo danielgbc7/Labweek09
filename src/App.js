f776c52df5bac2bab1450714e3e7370acd84f8fe
danielgbc7 <daniel.konjarski@georgebrown.ca>
2024-11-03 22:43:26 -0500

    lab week9

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>
+          Welcome to Fullstack Development - 1
+        </h1>
+        <h2>
+          React JS Programming Week09 Lab exercise
+        </h2>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          101436648
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Daniel Konjarski
+        </p>
+        <p>
+          George Brown College, Toronto
+        </p>
       </header>
     </div>
   );
